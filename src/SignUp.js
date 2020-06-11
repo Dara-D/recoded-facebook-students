c0a4b68abc5b9a9565b0921ca85dfa1c1b3199de
Your NameDara-D <dara.dandashi@gmail.com>
2020-06-11 20:39:33 +0300

    inisialCommit

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,8 +1,32 @@
 import React from "react";
-import db from "./firebase";
+// import db from "./firebase";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 const SignUpPage = () => {
-  return 'Your code goes here.';
+  return (
+    <Form>
+     <Button variant="outline-success" type="submit">
+      Log in with Google! 
+    </Button>
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>City</Form.Label>
+      <Form.Control type="text" placeholder="Write your city name" />
+    </Form.Group>
+  
+    <Form.Group controlId="formBasicPassword">
+      <Form.Label>Profile </Form.Label>
+      <Form.Control type="text" placeholder="Write something about you" />
+    </Form.Group>
+   
+    <Button variant="outline-success" type="submit">
+      join The Re:Coded facebook! 
+    </Button>
+  </Form> 
+  );
 };
 
 export default SignUpPage;
+
+ 
