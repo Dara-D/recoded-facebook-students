a4f04da631381a71a6c986956d805b230330d280
Your NameDara-D <dara.dandashi@gmail.com>
2020-06-18 22:59:58 +0300

    almost done

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,32 +1,80 @@
 import React from "react";
-// import db from "./firebase";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
+import db from "./firebase";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import firebase from "firebase";
 
 const SignUpPage = () => {
-  return (
-    <Form>
-     <Button variant="outline-success" type="submit">
-      Log in with Google! 
-    </Button>
-    <Form.Group controlId="formBasicEmail">
-      <Form.Label>City</Form.Label>
-      <Form.Control type="text" placeholder="Write your city name" />
-    </Form.Group>
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      console.log(result);
+      console.log("Success...!");
+    })
+    .catch(function (error) {
+      console.log(error);
+      console.log("This has failed :(");
+  });
+
   
-    <Form.Group controlId="formBasicPassword">
-      <Form.Label>Profile </Form.Label>
-      <Form.Control type="text" placeholder="Write something about you" />
-    </Form.Group>
-   
-    <Button variant="outline-success" type="submit">
-      join The Re:Coded facebook! 
-    </Button>
-  </Form> 
+  async function handleSubmit(props) {
+    // const res = await db.collection('profiles').get()
+    // const data = res.docs.map((el) => el.data());
+    // console.log(data)
+
+
+    // await db.collection("profiles").doc("userId").add({ imageUrl: "imageUrl"});
+
+    // await db
+    //   .collection("profiles").doc("FXFuISqWRMcsZmww8HI3M3WuFP02")
+    //   .update({
+    //     imageUrl: props.user.multiFactor.photoURL,
+    //     city: props.city,
+    //     name: props.user.b.displayName,
+    //     userId: props.user.u.uid,
+    //   });
+
+    // await db
+    //   .collection("profiles")
+    //   .doc("userId")
+    //   .update({ "city": profiles.city });
+
+    // db.collection("profiles").doc("userId").add({ user: user.displayname});
+    await db
+    .collection("profiles").doc("userId")
+    .update({
+      imageUrl: "imageUrl",
+      city: "city",
+      name: "name",
+      userId: "userId",
+    });
+    // // userId: The unique user ID (available via Google. Please don't make a user ID yourself).
+    // db.collection("profiles").doc("userId").add({ city: "city"});
+  }
+  handleSubmit();
+
+  return (
+    <Form >
+      <Button variant="outline-success" type="submit">
+        Log in with Google!
+      </Button>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>City</Form.Label>
+        <Form.Control type="text" placeholder="Write your city name" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Profile </Form.Label>
+        <Form.Control type="text" placeholder="Write something about you" />
+      </Form.Group>
+
+      <Button variant="outline-success" type="submit" onSubmit={handleSubmit}>
+        join The Re:Coded facebook!
+      </Button>
+    </Form>
   );
 };
 
 export default SignUpPage;
-
- 
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 import firebase from "firebase";
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "PUT_API_KEY_HERE",
+const firebaseConfig = {
+  apiKey: "AIzaSyDPFwcO5W4cSdCIBU4zYSWd2Hsi52CLNQs",
   authDomain: "recodedfacebook.firebaseapp.com",
   databaseURL: "https://recodedfacebook.firebaseio.com",
   projectId: "recodedfacebook",
@@ -16,3 +16,29 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 
 export default db;
+
+
+
+
+// const provider = new firebase.auth.GoogleAuthProvider();
+
+// // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+// firebase.auth().signInWithPopup(provider).then(function(result) {
+//   // This gives you a Google Access Token. You can use it to access the Google API.
+//   const token = result.credential.accessToken;
+//   // The signed-in user info.
+//   const user = result.user;
+//   // ...
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   const errorCode = error.code;
+//   const errorMessage = error.message;
+//   // The email of the user's account used.
+//   const email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   const credential = error.credential;
+//   // ...
+// });
+// firebase.auth().signInWithRedirect(provider);
+
